37a6a4a5af84e210bdf981cd7a2545fe2f33ec54
Palash <Palash.gupta@finoit.co.in>
2020-09-29 08:32:51 +0530

    mobile responsive layout

--- src/components/LayoutComponent/Layout.js
+++ src/components/LayoutComponent/Layout.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import classes from './Layout.css'
 import Aux from '../../hoc/auxillaryComp';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 
 import SideDrawer from '../../components/Navigation/Sidedrawer/Sidedrawer';
-const Layout = (props) =>
-  <Aux>
-    <Toolbar/>
-    <SideDrawer/>
-    <main className={classes.Content}>
-        {props.children}
-    </main>
-</Aux>
+class Layout extends Component {
+  state = {
+    showSideDrawer : false 
+  }
+  sideDrawerClosedHAndler = () =>{
+    this.setState({showSideDrawer : false })
+  }
+  sideDrawerToggleHandler = () =>{
+    this.setState((previousState)=>{
+      return {showSideDrawer:!previousState.showSideDrawer}
+  });
+}
+  render(){
+    return (
+      <Aux>
+      <Toolbar  drawerToggleClicked={this.sideDrawerToggleHandler}/>
+      <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHAndler}/>
+      <main className={classes.Content}>
+          {this.props.children}
+      </main>
+  </Aux>
+    )
+  }
+}
+ 
  
 export default Layout ;
--- src/components/Navigation/Sidedrawer/Sidedrawer.js
+++ src/components/Navigation/Sidedrawer/Sidedrawer.js
@@ -3,10 +3,19 @@ import React from 'react'
 import Logo from '../../Logo/Logo';
 import NavigationItem from '../NavigationFolder/NavigationFolder';
 import classes from './Sidedrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop' ;
+import Aux from '../../../hoc/auxillaryComp'
 
 const sideDrawer = (props)=>{
+    let attachedClasses  = [classes.SideDrawer , classes.Close]
+    if(props.open){
+        attachedClasses = [classes.SideDrawer , classes.Open]
+    }
+
     return (
-        <div className={classes.SideDrawer}>
+        <Aux>
+        <Backdrop show = {props.open} hideModal={props.closed}/>
+        <div className={attachedClasses.join(' ')}>
             <div className={classes.Logo}>
                  <Logo/>
             </div>
@@ -14,6 +23,7 @@ const sideDrawer = (props)=>{
                 <NavigationItem/>
             </nav>
         </div>
+        </Aux>
     );
 }
 
