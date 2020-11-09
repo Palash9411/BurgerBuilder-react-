5738dca4dea40981721cf740871a863d20b13728
Palash <Palash.gupta@finoit.co.in>
2020-11-09 09:40:24 +0530

    added spinner and api post hit

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/auxillaryComp';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props)=> {
-
-    const incredientSummary = Object.keys(props.incredients).map(igKey =>{
-    return <li key={igKey}>
-        <span 
-             style={{textTransform : 'capitalize'}}>
-        {igKey} </span> 
-             :{props.incredients[igKey]}
-        </li>
-    });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with following incredients:</p>
-            <ul>
-                {incredientSummary}
-            </ul>
-            <p><strong>Total Price : {props.price.toFixed(2)}$</strong></p>
-            <p>Continue to Checkout ?</p>
-            <Button btntype="Danger" clicked ={props.orderCancel}>CANCEL</Button>
-            <Button btntype="Success" clicked={props.orderContinue}>CONTINUE</Button>
-        </Aux>
-        
-    );
+class OrderSummary extends Component {
+   
+    render () {
+        const incredientSummary = Object.keys(this.props.incredients).map(igKey =>{
+            return <li key={igKey}>
+                <span 
+                     style={{textTransform : 'capitalize'}}>
+                {igKey} </span> 
+                     :{this.props.incredients[igKey]}
+                </li>
+            });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with following incredients:</p>
+                <ul>
+                    {incredientSummary}
+                </ul>
+                <p><strong>Total Price : {this.props.price.toFixed(2)}$</strong></p>
+                <p>Continue to Checkout ?</p>
+                <Button btntype="Danger" clicked ={this.props.orderCancel}>CANCEL</Button>
+                <Button btntype="Success" clicked={this.props.orderContinue}>CONTINUE</Button>
+            </Aux>
+            
+        );
+    }
 }
-export default orderSummary ;
+export default OrderSummary ;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from '../Modal/Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
 import Aux from '../../../hoc/auxillaryComp';
 
 
-const Modal = (props) => (
+class Modal extends Component{
+    shouldComponentUpdate(nextProps,nextState){
+        return nextProps.show !== this.props.show || nextProps.children !== this.props.children
+    }
+    componentWillUpdate(){
+        
+    }
+    
+    render (){
 
-    <Aux>
-        <Backdrop show={props.show} hideModal={props.modalClosed}/>
-        <div className={classes.Modal}
-        style={{
-            transform : props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity : props.show ? '1':'0'
-        }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+       
+        return (
+
+            <Aux>
+            <Backdrop show={this.props.show} hideModal={this.props.modalClosed}/>
+            <div className={classes.Modal}
+            style={{
+                transform : this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity : this.props.show ? '1':'0'
+            }}>
+                {this.props.children}
+            </div>
+        </Aux>
+        );
+    }
+}
 
 export default Modal
